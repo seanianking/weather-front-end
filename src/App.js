c71bd40b382170efd0dbae186f9084d01d1bff06
Sean <seanianking@gmail.com>
2024-03-03 11:21:19 -0700

    form accepts param and returns lat, lon

--- src/App.js
+++ src/App.js
@@ -1,34 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import WeatherQueryForm from './Components/WeatherQueryForm';
 import WeatherDisplaySection from './Components/WeatherDisplaySection';
 import axios from 'axios';
 
+
 const App = () => {
   const [weatherData, setWeatherData] = useState(null);
+  const [lat, setLat] = useState(null);
+  const [lon, setLon] = useState(null);
 
-  const searchWeather = async (location) => {
+  const getGeolocation = async (location) => {
     try {
-      // Add Nominatim API call to get latitude and longitude
-
-      // Add National Weather Service API call with obtained coordinates
-
-      // Update state with weather data
-
-      setWeatherData({
-        temperature: 75,
-        description: 'Sunny',
-        windSpeed: 10,
-        detailedForecast: 'Clear sky with no precipitation.',
-      });
+      const response = await axios.get(
+        `https://nominatim.openstreetmap.org/search?format=json&q=${location}&limit=1`
+      );
+      console.log(response.data[0]);
+      const { lat, lon } = response.data[0];
+      setLat(lat);
+      setLon(lon);
     } catch (error) {
-      console.error('Error fetching weather data:', error.message);
+      console.error('Error fetching geolocation:', error.message);
+      throw new Error('Invalid location');
     }
   };
+  console.log(lat, lon)
 
+
+  // useEffect(() => {
+  //   getGeolocation("Salt Lake City")
+  // }, [])
   return (
     <div>
       <h1>Weather App</h1>
-      <WeatherQueryForm onSearch={searchWeather} />
+      <WeatherQueryForm onSearch={getGeolocation} />
       <WeatherDisplaySection weatherData={weatherData} />
     </div>
   );
